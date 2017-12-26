452054b1fa1b879228d09f743303ec2a06eda24a
Satwik Singh <satwik@Satwiks-MacBook-Pro.local>
2017-12-26 14:49:16 +0530

    minor fixes

--- analyze.py
+++ analyze.py
@@ -1,15 +1,15 @@
 import json
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 \
-  import Features, CategoriesOptions
+  import Features, EntitiesOptions,EmotionOptions,KeywordsOptions,ConceptsOptions,SentimentOptions, CategoriesOptions
 
 natural_language_understanding = NaturalLanguageUnderstandingV1(
-  username='username',
-  password='password',
+  username='9b6a8482-b8a3-4f1b-8d76-08d5c65fc08a',
+  password='emH6VS7UXAAk',
   version='2017-02-27')
 
 response = natural_language_understanding.analyze(
-  url='www.ibm.com',
+  url='https://www.nytimes.com/2014/05/25/magazine/must-the-captain-always-go-down-with-the-ship.html',
   features=Features(
   	entities=EntitiesOptions(
       emotion=True,
