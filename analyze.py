d490873743940065f3d87fc216ab0ea6aea571ac
satwik <satwik.illini@gmail.com>
2018-03-17 20:56:44 -0500

    Separated api key, turned analyzer into a function

--- analyze.py
+++ analyze.py
@@ -1,32 +1,35 @@
 import json
 import os
+from apiKey import keyname, keypas, keyver
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 \
   import Features, EntitiesOptions,EmotionOptions,KeywordsOptions,ConceptsOptions,SentimentOptions, CategoriesOptions
 
 natural_language_understanding = NaturalLanguageUnderstandingV1(
-  username='9b6a8482-b8a3-4f1b-8d76-08d5c65fc08a',
-  password='emH6VS7UXAAk',
-  version='2017-02-27')
+  username= keyname,
+  password= keypas,
+  version= keyver)
 
-response = natural_language_understanding.analyze(
-  url='https://www.nytimes.com/2014/05/25/magazine/must-the-captain-always-go-down-with-the-ship.html',
-  features=Features(
-  	entities=EntitiesOptions(
-      emotion=True,
-      sentiment=True,
-      limit=15),
-  	emotion=EmotionOptions(
-      targets= ['keyword1','keyword2']),
-    keywords=KeywordsOptions(
-      emotion=True,
-      sentiment=True,
-      limit=2),
-    concepts=ConceptsOptions(
-      limit=5),
-    sentiment=SentimentOptions(
-      targets=['stocks']),
-    categories=CategoriesOptions()))
+def analyze(links):
+  for link in links:
+    response = natural_language_understanding.analyze(
+      url=link,
+      features=Features(
+      	entities=EntitiesOptions(
+          emotion=True,
+          sentiment=True,
+          limit=15),
+      	emotion=EmotionOptions(
+          targets= ['keyword1','keyword2']),
+        keywords=KeywordsOptions(
+          emotion=True,
+          sentiment=True,
+          limit=2),
+        concepts=ConceptsOptions(
+          limit=5),
+        sentiment=SentimentOptions(
+          targets=['stocks']),
+        categories=CategoriesOptions()))
 
 #Create json to store results of Watson analysis.
 dir = os.path.dirname(__file__)
